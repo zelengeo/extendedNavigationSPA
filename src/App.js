89deab1c96b40b79f601f0685a45635e759f8f85
Georgiy Zelenskiy <georgiy.zelenskiy@plus4u.net>
2019-01-22 09:12:21 +0100

    Initial commit from Create React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-
-import {BrowserRouter as Router, Link, Route} from "react-router-dom";
-import {Header} from "./components/Header";
-import {Home} from "./components/Home";
-import {About} from "./components/About";
-import {Footer} from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <div className={"routerBody"}>
-            <Header children={[(<Link to="/">Home</Link>),(<Link to="/about">About</Link>)]}/>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Footer/>
-          </div>
-        </Router>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
