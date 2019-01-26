7c6b1c6973d1412517ed9045f7d47674615c43c3
Georgiy Zelenskiy <pogapwnz@gmail.com>
2019-01-26 09:40:07 +0100

    Basics: router, navigation etc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import {Header} from "./components/Header";
+import {Home} from "./components/Home";
+import {About} from "./components/About";
+import {Footer} from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <div className={"routerBody"}>
+            <Header children={[(<Link to="/">Home</Link>),(<Link to="/about">About</Link>)]}/>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Footer/>
+          </div>
+        </Router>
       </div>
     );
   }
